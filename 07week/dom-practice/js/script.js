31297912263410f6338c5b831824210c9cbb6dfb
Heather Barr <hnbarr@yahoo.com>
2018-11-15 12:25:11 -0600

    new field and button added

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,41 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+window.onload=function() {
+  // alert user how many list items on page
+  let listTotal = document.getElementsByTagName('ul');
+  let message = listTotal[0].children.length;
+    alert(message);
+
+  // add h2 tag under title that says how many items in cart
+  let h2baby = document.createElement('h2');
+    h2baby.innerHTML = "You have " + listTotal[0].children.length + " items in your shopping cart.";
+    document.getElementsByTagName('h1')[0].after(h2baby);
+
+  // create ability to add more items to list
+  createNewField();
+  addItemsToExistingList();
+
+  function createNewField(){
+    let field = document.createElement('input');
+      field.setAttribute("id", 'item-text');
+      document.getElementsByTagName('ul')[0].after(field);
+    let inputfield = document.getElementById('item-text');
+      inputfield.setAttribute("placeholder", "Type to add Item");
+
+    let addbutton = document.createElement('button');
+      addbutton.setAttribute("id", 'new-button');
+      document.getElementById('item-text').after(addbutton);
+      addbutton.innerHTML = "add item";
+      addbutton.onclick = addItemsExistingList;
+
+  }
+
+  function addItemsToExistingList(event){
+    let userInput = document.getElementById('item-text').value;
+    return(addItemsToExistingList);
+
+  }
+
+
+
+};
