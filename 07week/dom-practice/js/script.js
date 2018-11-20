88b9501c46ccfcc3d0287e2dd5329ea54c8edea9
Heather Barr <hnbarr@yahoo.com>
2018-11-19 20:31:57 -0600

    working with remove button added to lis

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,43 +1,55 @@
 'use strict';
 
 window.onload=function() {
+
   let listTotal = document.getElementsByTagName('ul');
   let message = listTotal[0].children.length;
-    // alert(message);
+  // alert(message);
   let h2baby = document.createElement('h2');
-    h2baby.innerHTML = "You have " + listTotal[0].children.length + " items in your shopping cart.";
-    document.getElementsByTagName('h1')[0].after(h2baby);
+  h2baby.innerHTML = "You have " + listTotal[0].children.length + " items in your shopping cart.";
+  document.getElementsByTagName('h1')[0].after(h2baby);
 
   // create ability to add more items to list
-  createNewField()
-  function createNewField(){
-    let newInput = document.createElement('input');
-      newInput.setAttribute("id", 'item-text');
-      document.getElementsByTagName('ul')[0].after(newInput);
-      newInput.setAttribute("placeholder", "Type to add Item");
-    let addbutton = document.createElement('button');
-      addbutton.setAttribute("id", 'new-button');
-      document.getElementById('item-text').after(addbutton);
-      addbutton.innerHTML = "add item";
-      addbutton.onclick = addItemsToExistingList;
+  createNewField();
+  addingButtonToList();
+
+};
+
+
+function createNewField(){
+  let newInput = document.createElement('input');
+    newInput.setAttribute("id", 'item-text');
+    document.getElementsByTagName('ul')[0].after(newInput);
+    newInput.setAttribute("placeholder", "Type to add Item");
+  let addbutton = document.createElement('button');
+    addbutton.setAttribute("id", 'new-button');
+    document.getElementById('item-text').after(addbutton);
+    addbutton.innerHTML = "add item";
+    addbutton.onclick = addItemsToExistingList;
+}
+
+// add items to my list()
+function addItemsToExistingList(event){
+  let userInput = document.getElementById('item-text').value;
+  let textVal = document.createElement('li');
+  textVal.innerHTML = userInput;
+  document.getElementById('test').appendChild(textVal);
+  deleteButtonByEachLi(textVal);
+}
+
+//  append/create button on exitsing and new li's with forloop
+function deleteButtonByEachLi(appendElement){
+  let removeButton = document.createElement('button');
+    removeButton.setAttribute("id", "li-bye-bye");
+    removeButton.innerHTML = "Remove Item";
+    appendElement.appendChild(removeButton);
   }
 
-  // addItemsToExistingList()
-  function addItemsToExistingList(event){
-    let userInput = document.getElementById('item-text').value;
-    let textVal = document.createElement('li');
-    textVal.innerHTML = userInput;
-    document.getElementById('test').appendChild(textVal);
-    
-
-    // document.getElementsByTagName('ul').innerHTML = userInput;
-    // let keyWord = document.createTextNode(userInput).value;
-    // // textVal.appendChild(keyWord);
-    // textVal.appendChild(keyWord);
-    // document.getElementsByTagName('li')[0].after(userInput);
-    // not working...
-    // let textVal = document.getElementsByTagName('ul');
-    // userInput.innerHTML=('textVal');
-    // ul[0].innerHTML = ('userInput');
-  } 
-};
+  function addingButtonToList(){
+    let list = document.getElementById('test');
+    for(let i=0; i < list.children.length; i++){
+    deleteButtonByEachLi(list.children[i]);
+    }
+  }
+ 
+// now to remove items from my list with onclick, like add button function.
