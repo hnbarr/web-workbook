2462e37ae376e0824098c6b9657d7c07ecee6f2b
Heather Barr <hnbarr@yahoo.com>
2018-11-19 19:48:59 -0600

    working on add li

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,42 +1,43 @@
 'use strict';
 
 window.onload=function() {
-  // alert user how many list items on page
   let listTotal = document.getElementsByTagName('ul');
   let message = listTotal[0].children.length;
-    alert(message);
-
-  // add h2 tag under title that says how many items in cart
+    // alert(message);
   let h2baby = document.createElement('h2');
     h2baby.innerHTML = "You have " + listTotal[0].children.length + " items in your shopping cart.";
     document.getElementsByTagName('h1')[0].after(h2baby);
 
   // create ability to add more items to list
-  createNewField();
-  addItemsToExistingList();
-
+  createNewField()
   function createNewField(){
-    let field = document.createElement('input');
-      field.setAttribute("id", 'item-text');
-      document.getElementsByTagName('ul')[0].after(field);
-    let inputfield = document.getElementById('item-text');
-      inputfield.setAttribute("placeholder", "Type to add Item");
-
+    let newInput = document.createElement('input');
+      newInput.setAttribute("id", 'item-text');
+      document.getElementsByTagName('ul')[0].after(newInput);
+      newInput.setAttribute("placeholder", "Type to add Item");
     let addbutton = document.createElement('button');
       addbutton.setAttribute("id", 'new-button');
       document.getElementById('item-text').after(addbutton);
       addbutton.innerHTML = "add item";
-      addbutton.onclick = addItemsExistingList;
+      addbutton.onclick = addItemsToExistingList;
   }
 
-  function addItemsToExistingList(onclick){
+  // addItemsToExistingList()
+  function addItemsToExistingList(event){
     let userInput = document.getElementById('item-text').value;
-    // attampt to add item-text as a li onclick.
-    document.getElementsByTagName('li')[0].after(userInput);
-    // not working...
+    let textVal = document.createElement('li');
+    textVal.innerHTML = userInput;
+    document.getElementById('test').appendChild(textVal);
     
 
+    // document.getElementsByTagName('ul').innerHTML = userInput;
+    // let keyWord = document.createTextNode(userInput).value;
+    // // textVal.appendChild(keyWord);
+    // textVal.appendChild(keyWord);
+    // document.getElementsByTagName('li')[0].after(userInput);
+    // not working...
+    // let textVal = document.getElementsByTagName('ul');
+    // userInput.innerHTML=('textVal');
+    // ul[0].innerHTML = ('userInput');
   } 
-
-
 };
