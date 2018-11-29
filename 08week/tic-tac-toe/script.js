a58170f6738624ad795fc3f63445cd5fe6f94aed
Heather Barr <hnbarr@yahoo.com>
2018-11-28 19:58:51 -0600

    started tictactoe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  var turn = 'X';
+  $('[data-cell]').on('click',function(){
+    if($(this).text()=== ''){
+      $(this).text(turn);
+      checkForWin();
+      if(turn === 'O'){
+        turn = 'X';
+      }else{
+        turn = 'O';
+      }
+    }
+  })
+
+  function checkForWin(){
+    if($('[data-cell="0"]').text()===turn &&
+    $('[data-cell="1"]').text()===turn &&
+    $('[data-cell="2"]').text()===turn){
+    $('#announce-winner').text(`${turn} wins`);
+  }
+}
 });
