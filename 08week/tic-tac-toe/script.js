51a12dbd54a7947fafbca4601008c5a74a793c79
Heather Barr <hnbarr@yahoo.com>
2018-11-29 13:36:36 -0600

    clear working

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,21 +1,32 @@
 'use strict';
-//  how to switch turns
+//  how to switch turns WORKING!
 $(document).ready(function() {
   var turn = 'X';
   $('[data-cell]').on('click',function(){
     if($(this).text()=== ''){
       $(this).text(turn);
       checkForWin();
+      // count goes inside functin call above
       if(turn === 'O'){
         turn = 'X';
       }else{
         turn = 'O';
       }
     }
+    $('#clear').click (function(){
+      location.reload();
+    });
+    // clear board!!
   })
 
-// if theres a win at all...
+
+// if theres a win at all... WORKING!
   function checkForWin(){
+    // tie goes inside function call above
+    // var count = 0;
+    // if(tie === 9){
+    // $('#announce-winner').text(`Try Again!`);
+    // }
     if($('[data-cell="0"]').text()===turn &&
     $('[data-cell="1"]').text()===turn &&
     $('[data-cell="2"]').text()===turn){
@@ -56,10 +67,5 @@ $(document).ready(function() {
     $('[data-cell="6"]').text()===turn){
     $('#announce-winner').text(`${turn} wins`);
   }
-
-}
-
-
-
-
+  }
 });
