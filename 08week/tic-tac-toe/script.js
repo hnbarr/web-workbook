33131ee8e23ef793419be2bff54b0d2834b492e7
Heather Barr <hnbarr@yahoo.com>
2018-11-29 13:37:51 -0600

    all i need is tie

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
     if($(this).text()=== ''){
       $(this).text(turn);
       checkForWin();
-      // count goes inside functin call above
       if(turn === 'O'){
         turn = 'X';
       }else{
@@ -16,17 +15,12 @@ $(document).ready(function() {
     $('#clear').click (function(){
       location.reload();
     });
-    // clear board!!
+    // clear board WORKING!!
   })
 
 
 // if theres a win at all... WORKING!
   function checkForWin(){
-    // tie goes inside function call above
-    // var count = 0;
-    // if(tie === 9){
-    // $('#announce-winner').text(`Try Again!`);
-    // }
     if($('[data-cell="0"]').text()===turn &&
     $('[data-cell="1"]').text()===turn &&
     $('[data-cell="2"]').text()===turn){
