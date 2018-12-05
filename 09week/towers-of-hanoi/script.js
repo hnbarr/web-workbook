f7f995ce8872b91b6f71980714c7f9bfa2427dc9
Heather Barr <hnbarr@yahoo.com>
2018-12-05 17:03:20 -0600

    WORKING

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,32 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  console.log("hello");
+  // to drag blocks!
+    $('.draggable').draggable({
+      revert: 'invalid'
+  });
+  // to drop blocks into stacks!
+    $('.droppable').droppable({
+      drop: function ( event, ui ) {
+        let drag = $(ui.draggable).data('block');
+        console.log(drag);
+        let last = $(this).children().last().data('block');
+        console.log(last);
+        if (drag > last) {
+          $(ui.draggable).draggable('option', 'revert', true);
+        } else {
+          $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+        }
+        checkForWin();
+      }
+    })
+  // to check for winner!
+  function checkForWin (){
+    if ($('[data-stack="3"]').children().length === 4){
+      $('#announce').text("Congrats! You've completed the game!")
+  }
+  }
+  
+
 });
